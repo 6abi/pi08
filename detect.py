2cd7ea2a94d405cf605a1ad4c92dac408ba4f28c
Barbara <6abi.cardoso@gmail.com>
2021-11-16 21:35:01 -0300

    processo totalmente automatizado

--- detect.py
+++ detect.py
@@ -19,7 +19,7 @@ flags.DEFINE_integer('size', 416, 'resize images to')
 flags.DEFINE_boolean('tiny', True, 'yolo or yolo-tiny')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 flags.DEFINE_string('image', './data/test_defeito.jpg', 'path to input image')
-flags.DEFINE_string('output', './result.png', 'path to output image')
+flags.DEFINE_string('output', './data/result.png', 'path to output image')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.60, 'score threshold')
 
